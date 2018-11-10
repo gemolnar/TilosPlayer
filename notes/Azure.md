4cc3f7f3ac57c220aa84b232612187decf342920
Gergo Molnar <gerely@gmail.com>
2018-11-10 09:27:29 +0100

    Code and note cleanup

---
+++ notes/Azure.md
@@ -0,0 +1,8 @@
+
+
+Azure Storage:
+https://stackoverflow.com/questions/47218362/microsoft-azure-app-service-storage
+
+Kudu:
+https://tilosplayer.scm.azurewebsites.net/
+
--- src/Website/Controllers/HomeController.cs
+++ src/Website/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace TilosPlayer.Website.Controllers
         public async Task<IActionResult> Index(int w)
         {
             var api = new Api();
-            var now = DateTimeOffset.UtcNow;
+            var now = DateTimeOffset.UtcNow; 
             var episodes = await api.GetEpisodesByTimeRange(now.AddDays(-7 * (w+1)), now.AddDays(-7*w));
             var shows = await api.GetAllShows();
             foreach (var episode in episodes)
@@ -36,57 +36,5 @@ namespace TilosPlayer.Website.Controllers
             episodes = episodes.OrderByDescending(e => e.RealFrom).ToArray();
             return View(episodes);
         }
-
-        //public async Task<IActionResult> Index()
-        //{
-        //    //TimeZoneInfo.ConvertTime(DateTime.UtcNow, TimeZoneInfo.GetSystemTimeZones)
-
-
-
-        //    //var source = "https://tilos.hu/feed/podcast";
-        //    var baseDir = WebRootPath;
-        //    var source = string.Concat(baseDir, @"\weekly.atom.xml");
-        //    //var source = @"C:\Users\geri\source\repos\tilos\tilos\podcast.atom.xml";
-        //    try
-        //    {
-        //        var atomReader = new AtomFeedReader();
-        //        var atomFeedModel = await atomReader.ParseAtomFeed(source);
-        //        var tilosFeedModel = new TilosPodcastModel(atomFeedModel);
-        //        var xy = tilosFeedModel.Items;
-        //        var httpClient = new HttpClient();
-
-        //        //return Json(tilosFeedModel, new Newtonsoft.Json.JsonSerializerSettings() { Formatting  = Newtonsoft.Json.Formatting.Indented });
-        //        return View(tilosFeedModel);
-        //    }
-        //    catch(FileNotFoundException fex)
-        //    {
-        //        return Json(new { Files = Directory.GetFileSystemEntries(Path.GetDirectoryName(source)), Root = source, Ex = fex.Message, ExFile = fex.FileName });
-        //    }
-        //}
-
-        //public IActionResult About()
-        //{
-        //    ViewData["Message"] = "Your application description page.";
-
-        //    return View();
-        //}
-
-        //public IActionResult Contact()
-        //{
-        //    ViewData["Message"] = "Your contact page.";
-
-        //    return View();
-        //}
-
-        //public IActionResult Privacy()
-        //{
-        //    return View();
-        //}
-
-        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        //public IActionResult Error()
-        //{
-        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        //}
     }
 }
